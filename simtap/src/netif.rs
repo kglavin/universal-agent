2670e309b6fa3118864a4fe9cc8c978841ffdbd1
kglavin <kglavin@gmail.com>
2019-10-14 14:23:03 -0700

    Update netif.rs
    initial stab at netif for mac and linux

--- simtap/src/netif.rs
+++ simtap/src/netif.rs
@@ -1,1 +1,109 @@
 // network interface abstraction using tun/tap as appropriate across the various platforms
+
+#[cfg(target_os = "macos")]
+use std::net::UdpSocket;
+
+#[cfg(target_os = "linux")]
+use tun_tap;
+
+
+#[cfg(target_os = "macos")]
+//struct MacosInterface<UdpSocket, String> {
+struct MacosInterface {  
+	fd: UdpSocket,
+	if_name: String,
+}
+
+#[cfg(target_os = "macos")]
+impl MacosInterface { 
+	fn send(&self, buf: &[u8]) -> std::io::Result<usize> { 
+		self.fd.send(buf)
+	}
+
+	fn recv(&self, mut buf: &mut [u8]) -> std::io::Result<usize> { 
+		self.fd.recv(&mut buf)
+	}
+
+	fn name(&self) -> &str { 
+		&self.if_name
+	}
+}
+
+#[cfg(target_os = "linux")]
+struct LinuxInterface { 
+	netif: tun_tap::Iface,
+}
+
+#[cfg(target_os = "linux")]
+impl LinuxInterface { 
+	fn send(&self, buf: &[u8]) -> Result<usize> { 
+		self.netif.send(buf)
+	}
+	fn recv(&self, buf: &mut[u8]) -> Result<usize> { 
+		self.netif.recv(&mut buf)
+	}
+		fn name(&self) -> &str { 
+			self.netif.name()
+	}
+
+}
+
+
+//#[cfg(target_os = "macoss")]
+//impl <MacosInterface> Interface<MacosInterface> {
+//    pub fn new(nic: MacosInterface) -> Self {
+//		Interface { 
+//			nic: nic,
+//			}
+//		}
+//    }
+//#[cfg(target_os = "linux")]
+//impl <LinuxInterface> Interface<LinuxInterface> {
+//    pub fn new(nic: LinuxInterface) -> Self {
+//		Interface { 
+//			nic: nic,
+//			}
+//		}
+//    }
+
+impl <T> Interface<T> {
+    pub fn new(nic: T) -> Self {
+		Interface { 
+			nic: nic,
+			}
+		}
+    }
+
+pub struct Interface<T> {
+	nic: T,
+}
+
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+#[test]
+
+#[cfg(target_os = "macos")]
+    fn test_1_macos() {
+		let (fd,if_name) = mac_utun::get_utun().expect("Error, did not get a untun returned");
+		let a = MacosInterface { fd, if_name } ;
+    	let b = Interface::new(a);
+
+    	println!("fd = {:?}",b.nic.fd);
+    	assert_eq!(b.nic.if_name, b.nic.if_name)
+
+    }
+#[cfg(target_os = "linux")]
+    fn test_1_linux() {
+    	let name = "utun1";
+    	let netif = tun_tap::Iface::without_packet_info(name, tun_tap::Mode::Tun)?;
+
+    	let a = LinuxInterface { netif } ;
+    	let b = Interface::new(a);
+    }
+
+
+
+
+}
